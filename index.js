9a7bebf8665892a3adaf4ac82a89fb25f157d994
NawlFountains <56007803+NawlFountains@users.noreply.github.com>
2023-10-10 17:15:32 -0300

    Update tax fee

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { Client, IntentsBitField, messageLink } = require('discord.js');
 const { token, serverIP, serverPort } = require('./config.json');
 const mcp = require('minecraft-protocol');
-const impuestos = 1.76; //21 IVA + 9 Servicio digital + 45 Impuesto Pais
-const impuestosExtras = impuestos+ 0.21; //Bienes personales 21
+const impuestos = 2.00; //30 Pais + 45 Impuesto Ganancias + unificacion
+//const impuestosExtras = impuestos+ 0.21; //Bienes personales 21
 const channelId = "750481919137022102"; // #jf
 
 const client = new Client({
@@ -29,13 +29,11 @@ client.on('messageCreate', async (msg) => {
     if (msg.content.startsWith('!pais ')){
         var priceStringHolder = msg.content.split(' ');
         var price = parseFloat(priceStringHolder[1]);
-        msg.reply('Se aplican los siguientes impuestos: \n**IVA 21% **: $'+ (price*(0.21)).toFixed(2) 
-                    +' \n**Servicios digitales 9% **: $'+ (price*(0.09)).toFixed(2) 
-                    +'\n**Impuesto Pais 45% **: $'+ (price*(0.45)).toFixed(2) 
-                    +' \nPrecio final es $'+ (price*impuestos).toFixed(2) 
-                    + " \n\nRecorda que si gastas mas de 300 USD por mes se te aplica \n**Bienes personales 21% **: $"+ (price*0.21).toFixed(2) 
-                    +" \nen ese caso el total es $" + (price*impuestosExtras).toFixed(2) 
-                    +" pesos. \n\n_Si no pagas ganancias podes pedir la devolucion del impuesto PAIS por al pagina de la AFIP._");
+        msg.reply('Precio final es $'+ (price*impuestos).toFixed(2) 
+                    +' pesos. \n'
+		    +'\nSi vivis en una provincia la que aplica el impuesto a sellos sobre las tarjetas de credito entonces se te suma el importe del mismo 1,2%,'+
+        '\nen este caso es de $'+ (price*(0.012)).toFixed(2)
+		    +"\n\n_Si no pagas ganancias podes pedir la devolucion de la percepcion a las ganancias por al pagina de la AFIP._");
     } else if (msg.content.startsWith("!server")) {
         mcp.ping({ host: serverIP, port: serverPort }, (err, results) => {
             if (err) {
